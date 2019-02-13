0893bfbe6eae79f373f9f406d21df9b0b1a8b8bf
NhatMinh Nguyen <nhatminhn900@gmail.com>
2019-02-13 22:38:55 +0700

    fix cors

--- app.js
+++ app.js
@@ -26,12 +26,6 @@ mongoose.Promise = global.Promise;
 // set middleware
 const authCheckMiddleware = require('./middleware/auth-check');
 
-// connect to local  MongoDB
-// mongoose.connect('mongodb://localhost/product_banle_beta')
-//     .then(()=>  console.log('connection to nong-nghiep succesful'))
-// .catch((err) => console.error(err));
-
-// connect to Server MongoDB
 
 var configDB = require('./config/database.js');
 mongoose.connect(configDB.urlHeroku)
@@ -40,7 +34,26 @@ mongoose.connect(configDB.urlHeroku)
 
 
 var app = express();
-app.use(cors({origin: 'http://localhost:3000'}));
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // Config auth
 var flash = require('connect-flash');
 var session = require('express-session');
