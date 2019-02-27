e6c13c6439096de462254daee3f848ba91be20a7
Minh Nguyen Nhat <minh.nguyennhat@axonactive.com>
2019-02-27 11:50:05 +0700

    added env file

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
     jwtSecret: "MyS3cr3tK3Y",
     jwtSession: {
         session: false
     },
-    AWS_ACCESS_KEY_ID: "",
-    AWS_SECRET_ACCESS_KEY: "",
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
 };
--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
+require('dotenv').config()
 module.exports = {
-  urlLocal    : 'mongodb://localhost/product_banle_beta',
-  urlHeroku   : 'mongodb://heroku_n1w7rslr:m3f4hhv8vb2hsgfp5rq1v3p5r4@ds129484.mlab.com:29484/heroku_n1w7rslr',
+  urlLocal    :  process.env.URL_DB_LOCAL,
+  urlHeroku   : process.env.URL_DB_HEROKU
 };
