3013324547d5e24b25d1075d2ad7a73684b81964
Minh Nguyen Nhat <minh.nguyennhat@axonactive.com>
2019-02-27 13:42:08 +0700

    debug upload images

--- routes/image_upload.js
+++ routes/image_upload.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const upload = require('../services/image_upload');
-
+const config = require('../config/config');
 const singleUpload = upload.single('image')
 
 router.post('/', function(req, res) {
-    console.log(' ******  Test upload image  ***** ')
+    console.log(' ******  Upload image  ',config.AWS_ACCESS_KEY_ID, "AWS_SECRET_ACCESS_KEY  ==> ",config.AWS_SECRET_ACCESS_KEY)
 
     singleUpload(req, res, function(err, some) {
         if (err) {
