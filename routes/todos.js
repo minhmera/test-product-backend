e585c584cd9bfeb1ca182087ad129f82ffb37f05
Nhat Minh <nhatminhn900@gmail.com>
2020-09-11 09:37:00 +0700

    added mock rewards api

--- routes/todos.js
+++ routes/todos.js
@@ -3,8 +3,45 @@ var router = express.Router();
 
 var Todo = require('../models/Todo.js');
 
+
+let rewardJson = [
+  {
+    'name': 'Free ship under 20k',
+    'validDate': new Date('2020-12-21T03:24:00'),
+    'quantity':1,
+  },
+  {
+    'name': 'Discount 30k',
+    'validDate': new Date('2020-12-22T03:24:00'),
+    'quantity':1,
+  },
+  {
+    'name': 'Free Desert',
+    'validDate': new Date('2020-12-23T03:24:00'),
+    'quantity':1,
+  },
+  {
+    'name': 'Bonus voucher 200k ',
+    'validDate': new Date('2020-12-24T03:24:00'),
+    'quantity':1,
+  },
+  {
+    'name': 'Free for children',
+    'validDate': new Date('2020-12-25T03:24:00'),
+    'quantity':1,
+  },
+
+];
+
+
+router.get('/mockTest', function (req, res, next) {
+  //console.log('locationVNData  ==> ', locationVNData)
+  res.json(rewardJson );
+});
+
+
 /* GET /todos listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   Todo.find(function (err, todos) {
     if (err) return next(err);
     res.json(todos);
@@ -12,17 +49,17 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST /todos */
-router.post('/', function(req, res, next) {
-  console.log('****   req  ',req.body)
+router.post('/', function (req, res, next) {
+  console.log('****   req  ', req.body);
   Todo.create(req.body, function (err, post) {
     if (err) return next(err);
-    console.log('****   user  ',post)
+    console.log('****   user  ', post);
     res.json(post);
   });
 });
 
 /* GET /todos/id */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
   Todo.findById(req.params.id, function (err, post) {
     if (err) return next(err);
     res.json(post);
@@ -30,7 +67,7 @@ router.get('/:id', function(req, res, next) {
 });
 
 /* PUT /todos/:id */
-router.put('/:id', function(req, res, next) {
+router.put('/:id', function (req, res, next) {
   Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
@@ -38,7 +75,7 @@ router.put('/:id', function(req, res, next) {
 });
 
 /* DELETE /todos/:id */
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function (req, res, next) {
   Todo.findByIdAndRemove(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
