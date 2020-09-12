e564518cf297a426083a1365f095c733ceab5a2c
Nhat Minh <nhatminhn900@gmail.com>
2020-09-12 16:44:28 +0700

    removed bcrypt-nodejs and upgrade nodejs version to 12.10.0

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+//var bcrypt   = require('bcrypt-nodejs');
+var bcrypt   = require('bcrypt')
 
 var userSchema = mongoose.Schema({
     local: {
