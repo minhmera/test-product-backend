9e48d6f5345837fb347d5b96a1931de041527954
Nhat Minh <nhatminhn900@gmail.com>
2020-10-11 19:29:41 +0700

    change config db file

--- config/database.js
+++ config/database.js
@@ -3,5 +3,7 @@ require('dotenv').config()
 module.exports = {
   urlLocal    :  process.env.URL_DB_LOCAL,
   urlHeroku   : process.env.URL_DB_HEROKU
+  //urlHeroku   : "Define here"
+
 };
 /** Using local **/
